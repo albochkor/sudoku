48d2b1381df70c4f3c18f43e4249d5c5a232234a
gherage <gerich89@gmail.com>
2023-09-15 12:15:31 +0300

    read

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,26 @@
 module.exports = {
   env: {
+    browser: true,
     commonjs: true,
     es2021: true,
-    node: true,
-    jest:true,
   },
-  extends: [
-    'airbnb-base',
+  extends: 'airbnb-base',
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [
+        '.eslintrc.{js,cjs}',
+      ],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
   ],
   parserOptions: {
     ecmaVersion: 'latest',
   },
   rules: {
-    'no-console': 0,
   },
 };
